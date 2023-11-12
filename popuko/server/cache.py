57f0ad00100bcebaa1bf4d40333a34329821c948
su <soodoshll@gmail.com>
2023-11-12 00:45:23 -0500

    update

--- popuko/server/cache.py
+++ popuko/server/cache.py
@@ -1,4 +1,4 @@
-from typing import List, Optional
+from typing import List, Optional, Tuple
 import transformers
 import torch
 
@@ -6,38 +6,73 @@ class Node:
     def __init__(self,
                  token_ids: List[int],
                  kv_cache: torch.Tensor, 
-                 logits: torch.Tensor,
+                #  logits: torch.Tensor,
                  parent: Optional['Node'] = None):
         self.token_ids = token_ids
         self.kv_cache = kv_cache
-        self.logits = logits
+        # self.logits = logits
         self.parent = parent
         self.children = []
 
+def index_kvcache(kv_cache, start, end):
+    return [[kv[:, start:end].clone() for kv in layer] for layer in kv_cache]
+
+def concat_kvcache(kv_cache_list):
+    assert len(kv_cache_list) > 0
+    # concat kv cache
+    kv_tensors = []
+    for layer in range(len(kv_cache_list[0])):
+        layer_kv = []
+        for k_or_v in range(2):
+            kv = [kv_cache[layer][k_or_v] for kv_cache in kv_cache_list]
+            # print(kv)
+            kv = torch.cat(kv, dim=1)
+            layer_kv.append(kv)
+        kv_tensors.append(layer_kv)
+    return kv_tensors
+
+
 class TrieCache:
     def __init__(self):
-        self.root : List[Node] = []
+        self.root = Node([], None, None)
 
-    def match(self, seq: List[int]) -> Optional[Node]:
-        def _match(node : Node, seq) -> Optional[Node]:
-            seq_len = len(node.token_ids)
-            if len(seq) <= seq_len:
+    def lookup(self, seq: List[int], partial=True) -> Tuple[Optional[Node], List[torch.Tensor]]:
+        def _lookup(node : Node, seq, partial) -> Tuple[Optional[Node], List[torch.Tensor]]:
+            node_seq_len = len(node.token_ids)
+            # print(node, seq)
+            if partial and len(seq) <= node_seq_len:
                 if seq == node.token_ids[:len(seq)]:
-                    return node
+                    return node, [index_kvcache(node.kv_cache, 0, len(seq))]
                 else:
-                    return None
-            if seq[:seq_len] == node.token_ids:
-                tail = seq[seq_len:]
-                for child in self.children:
-                    ret = _match(child, tail)
+                    return None, []
+            if seq[:node_seq_len] == node.token_ids:
+                tail = seq[node_seq_len:]
+                node_kv = [] if node == self.root else [node.kv_cache]
+                child_kv = []
+                ret = self.root
+                for child in node.children:
+                    ret, child_kv = _lookup(child, tail, partial)
                     if ret is not None:
-                        return ret
-            return None
-        for child in self.root:
-            ret = _match(child, seq)
-            if ret is not None:
-                return ret
-        return None
+                        return ret, node_kv + child_kv
+                return node, node_kv
+            return None, []
+        return _lookup(self.root, seq, partial) 
+
+    def path_len(self, node: None) -> int:
+        path_len = 0
+        while node is not None:
+            path_len += len(node.token_ids)
+            node = node.parent
+        return path_len
+
+    def insert(self, token_ids, kv_cache):
+        insert_point, _ = self.lookup(token_ids, partial=False)
+        prefix_len = self.path_len(insert_point)
+        if prefix_len == len(token_ids):
+            return
+        new_node = Node(token_ids[prefix_len:], index_kvcache(kv_cache, -prefix_len, None), insert_point)
+        insert_point.children.append(new_node)
+
 
 if __name__ == '__main__':
     model = transformers.AutoModelForCausalLM.from_pretrained('gpt2')
--- popuko/server/server.py
+++ popuko/server/server.py
@@ -12,7 +12,7 @@ import argparse
 import transformers
 import torch
 
-from .cache import TrieCache
+from .cache import TrieCache, concat_kvcache, index_kvcache
 
 KVCache = Tuple[Tuple[torch.Tensor]]
 # [Layer, kv, batchsize, num_heads, seqlen, dim]
@@ -28,6 +28,7 @@ class Request:
         # Runtime info
         self._generated_tokens = []
         self._event = threading.Event()
+        self._trie_node = None
     
     def add_token(self, token: int):
         self._generated_tokens.append(token)
@@ -48,8 +49,13 @@ class Request:
         return len(self.prompt) + len(self._generated_tokens) - self.kv_len()
     
     def input_ids(self):
-        return (self.prompt + self._generated_tokens)[-self.input_ids_len():]
+        return (self.prompt + self._generated_tokens)[self.kv_len():]
+    
+    def tokens(self):
+        return self.prompt + self._generated_tokens
 
+    def has_cache(self) -> bool:
+        return self.kv_cache is not None
 
 class PopukoServer:
     def __init__(self, model, max_batch_size:int=16, device='cuda', use_cache=True):
@@ -58,7 +64,8 @@ class PopukoServer:
         self.max_batch_size: int = max_batch_size
         self.finished = False
 
-        self.trie_cache = TrieCache
+        self.trie_cache = TrieCache()
+
         self.device = device
         self.use_cache = use_cache
 
@@ -69,34 +76,54 @@ class PopukoServer:
         self.server_thread = threading.Thread(target=self._main_loop)
         self.server_thread.start()
     
-    def find_kv_cache(self, req: Request) -> KVCache:
+    def find_kv_cache(self, req: Request):
         # look up kv cache in the trie
-        return req.kv_cache
+        _, kv_cache = self.trie_cache.lookup(req.tokens())
+        if len(kv_cache) > 0:
+            # print("CACHE HIT")
+            kv_cache = concat_kvcache(kv_cache)
+            req.update_kv_cache(kv_cache)
+            if req.input_ids_len() <= 0:
+                kv_cache = index_kvcache(kv_cache, None, -1)
+                req.update_kv_cache(kv_cache)
+
+    def cache_write_back(self, req):
+        # find again, if node already exists, skip
+        self.trie_cache.insert(req.tokens()[:-1], req.kv_cache)
+
 
     def process_batch(self, reqs, output):
         new_reqs = []
         for i, req in enumerate(reqs):
-            next_token = int(output.logits[i, req.input_ids_len() - 1].argmax(dim=-1).cpu())
+            next_token = int(output.logits[i, -1].argmax(dim=-1).cpu())
+            # print(f"req {i}, next_token {next_token}")
+            if next_token == 0:
+                raise RuntimeError('Unexpected token generated')
+                # print(output.logits[i, -1])
+                # exit()
             req.add_token(next_token)
             if req.finished():
-                req.update_kv_cache(None)
-                torch.cuda.empty_cache()
+                if self.use_cache:
+                    self.cache_write_back(req)
+                    req.update_kv_cache(None)
                 req._event.set()
             else:
                 new_reqs.append(req)
                 # update kv_cache
                 if self.use_cache:
-                    seq_len = req.input_ids_len() - 1
-                    new_kv = [[kv[i, :, :seq_len, ].clone() for kv in layer] for layer in output.past_key_values]
+                    num_input_ids = req.input_ids_len() - 1
+                    if req.has_cache():
+                        new_kv = [[kv[i, :, -num_input_ids:, ].clone() for kv in layer] for layer in output.past_key_values]
+                        new_kv = concat_kvcache([req.kv_cache, new_kv])
+                    else:
+                        new_kv = [[kv[i, :, -num_input_ids:, ].clone() for kv in layer] for layer in output.past_key_values]
                     req.update_kv_cache(new_kv)
-        # del output.past_key_values
-        torch.cuda.empty_cache()
         return new_reqs
 
-    def build_batch(self, reqs):
+    def build_batch(self, reqs, non_blocking=False):
         batch_size = len(reqs)
         kv_cache = [req.kv_cache for req in reqs]
-        kv_len_list = [0 if req_cache is None else req_cache[0][0].shape[1] for req_cache in kv_cache]
+        kv_len_list = [req.kv_len() for req in reqs]
 
         # Concatenete kv_cache
         start_t = time.time()
@@ -111,39 +138,47 @@ class PopukoServer:
             num_layers = len(kv_example)
             kv_shapes = [kv_example[0][i].shape for i in range(2)]
 
+            # print(len(kv_example), len(kv_example[0]), kv_example[0][0].shape) 
+
             kv_cache_tensor = []
             for layer in range(num_layers):
                 layer_kv = []
                 for k_or_v in range(2):
                     shape = kv_shapes[k_or_v]
                     num_heads, _, dim = shape
-                    kv_tensor = torch.empty((batch_size, num_heads, max_kv_cache_len, dim), device=self.device, dtype=dtype)
+                    kv_tensor = torch.zeros((batch_size, num_heads, max_kv_cache_len, dim), device=self.device, dtype=dtype)
                     for req_id in range(batch_size):
                         if kv_cache[req_id] is not None:
                             seqlen = kv_len_list[req_id]
-                            kv_tensor[req_id, :, :seqlen].copy_(kv_cache[req_id][layer][k_or_v], non_blocking=True)
+                            kv_tensor[req_id, :, :seqlen].copy_(kv_cache[req_id][layer][k_or_v], non_blocking=non_blocking)
                     layer_kv.append(kv_tensor)
                 kv_cache_tensor.append(layer_kv)
         self.build_kv_time += time.time() - start_t 
 
         # Concatenate input ids
-        input_ids_list = [(req.prompt + req._generated_tokens)[kv_len:] for req, kv_len in zip(reqs, kv_len_list)]
+        input_ids_list = [req.input_ids() for req in reqs]
         max_input_len = max([len(input_ids) for input_ids in input_ids_list])
+        # print([len(input_ids) for input_ids in input_ids_list])
 
         input_ids_tensor = torch.zeros((batch_size, max_input_len), device=self.device, dtype=torch.long)
         for i, input_ids in enumerate(input_ids_list):
-            input_ids_tensor[i, :len(input_ids)].copy_(torch.tensor(input_ids, device=self.device), non_blocking=True)
+            assert(len(input_ids) > 0)
+            input_ids_tensor[i, -len(input_ids):].copy_(torch.tensor(input_ids, device=self.device), non_blocking=non_blocking)
 
         # Construct mask
         attn_mask = torch.ones([batch_size, max_kv_cache_len + max_input_len], device=self.device, dtype=torch.long)
         for i, input_ids in enumerate(input_ids_list):
-            attn_mask[i, max_kv_cache_len + len(input_ids):] = 0
-            attn_mask[i, kv_len_list[i] : max_kv_cache_len] = 0
-
+            # inputs left padding
+            # kv right padding
+            attn_mask[i, reqs[i].kv_len() : -len(input_ids)] = 0
+        # print(input_ids_tensor)
+        # if kv_cache_tensor is not None:
+            # print(kv_cache_tensor[0][0][:, 0, :, 0])
+        # print(attn_mask)
         torch.cuda.synchronize()
         return input_ids_tensor, attn_mask, kv_cache_tensor
     
-    # @torch.inference_mode()
+    @torch.inference_mode()
     def run_model(self, *args, **kwargs):
         ret = self.model(*args, **kwargs)
         torch.cuda.synchronize()
@@ -151,7 +186,6 @@ class PopukoServer:
 
     @torch.inference_mode()
     def _main_loop(self):
-        print('The server is launched')
         req_buf = []
         while not self.finished or len(req_buf) > 0:
             # get the next batch
@@ -167,10 +201,10 @@ class PopukoServer:
                     print('server exiting')
                     self.finished = True
                 else:
+                    if self.use_cache:
+                        self.find_kv_cache(req)
                     req_buf.append(req)
-                    # print('recv', id(req))
             if len(req_buf) == 0:
-                print('exiting')
                 break
             input_ids, attn_mask, kv_cache_tensor = self.build_batch(req_buf)
             output = self.run_model(input_ids=input_ids, 
@@ -197,21 +231,33 @@ async def _main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--cache", action='store_true')
     parser.add_argument("--check", action='store_true')
+    parser.add_argument("--model", type=str, default='facebook/opt-1.3b')
     args = parser.parse_args()
-    model = transformers.AutoModelForCausalLM.from_pretrained('gpt2-large')
+    model = transformers.AutoModelForCausalLM.from_pretrained(args.model)
+    tokenizer = transformers.AutoTokenizer.from_pretrained(args.model)
     model.half().to('cuda')
-    tokenizer = transformers.AutoTokenizer.from_pretrained('gpt2-large')
+    # gpt2 is not supported
     server = PopukoServer(model=model, use_cache=args.cache)
     server.launch()
-    input_ids = tokenizer('Tell me something about the university of Toronto').input_ids
+    input_ids = tokenizer(
+        "This will be a very long prompt. So first of all, please tell me something about the university of Toronto. "
+        "I'm mostly interested in its research reputation in the world, especially about Computer Science."
+        ).input_ids
+    
+    if args.check:
+        ground_truth = model.generate(torch.tensor([input_ids], device='cuda', dtype=torch.long), max_new_tokens=30, min_new_tokens=30)
+        ground_truth = tokenizer.batch_decode(ground_truth)
+        print(ground_truth)
+
     tasks = []
+    torch.cuda.synchronize()
     start_t = time.time()
     for i in range(100):
         await asyncio.sleep(0.1)
-        tasks.append(server.request(input_ids, 100))
+        tasks.append(asyncio.create_task(server.request(input_ids, 30)))
     reqs = await asyncio.gather(*tasks)
+    print('time elapsed:', time.time() - start_t, server.build_kv_time)
     server.stop()
-    print(time.time() - start_t, server.build_kv_time)
     if args.check:
         for req in reqs:
             print(tokenizer.decode(req._generated_tokens))
